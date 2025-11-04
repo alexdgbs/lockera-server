e21b243a13b8dbd9178bd6b30b962b4b79da2878
alexdgbs <alexdbc24@gmail.com>
2025-11-03 18:58:07 -0600

    lol

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 app.use(cors({
-  origin: 'https://lockera.vercel.app',
+  origin: '*',
   credentials: true
 }));
 
